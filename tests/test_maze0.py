6ed13e5ebb8d3724b28d4ccef5f9c9a4c94de170
Michal Převrátil <michprev@gmail.com>
2025-05-19 18:51:36 +0200

    ✨ Add Wake tests

---
+++ tests/test_maze0.py
@@ -0,0 +1,54 @@
+from wake.testing import *
+from wake.testing.fuzzing import *
+
+from pytypes.generatedmazes.maze0_wake import Maze
+
+def random_uint64() -> int:
+    bits = random.randint(1, 64)
+    return random.getrandbits(bits)
+
+
+discovered = set()
+
+
+class Maze0FuzzTest(FuzzTest):
+    maze: Maze
+
+    def pre_sequence(self) -> None:
+        self.maze = Maze.deploy()
+
+
+    @flow()
+    def flow_move_north(self) -> None:
+        with may_revert():
+            tx = self.maze.moveNorth(*[random_uint64() for _ in range(8)])
+
+    @flow()
+    def flow_move_south(self) -> None:
+        with may_revert():
+            tx = self.maze.moveSouth(*[random_uint64() for _ in range(8)])
+
+    @flow()
+    def flow_move_east(self) -> None:
+        with may_revert():
+            tx = self.maze.moveEast(*[random_uint64() for _ in range(8)])
+
+    @flow()
+    def flow_move_west(self) -> None:
+        with may_revert():
+            tx = self.maze.moveWest(*[random_uint64() for _ in range(8)])
+
+    @invariant(period=100)
+    def invariant_found(self) -> None:
+        discovered.update([i for i in range(1, 49) if self.maze.found(i)])
+        print(discovered)
+        #print("Found ", [i for i in range(1, 49) if self.maze.found(i)])
+
+
+
+@chain.connect()
+def test_maze0():
+    try:
+        test = Maze0FuzzTest().run(100, 1_000_000)
+    finally:
+        print("Discovered ", discovered)
---
+++ tests/test_maze1.py
@@ -0,0 +1,54 @@
+from wake.testing import *
+from wake.testing.fuzzing import *
+
+from pytypes.generatedmazes.maze1_wake import Maze
+
+def random_uint64() -> int:
+    bits = random.randint(1, 64)
+    return random.getrandbits(bits)
+
+
+discovered = set()
+
+
+class Maze1FuzzTest(FuzzTest):
+    maze: Maze
+
+    def pre_sequence(self) -> None:
+        self.maze = Maze.deploy()
+
+
+    @flow()
+    def flow_move_north(self) -> None:
+        with may_revert():
+            tx = self.maze.moveNorth(*[random_uint64() for _ in range(8)])
+
+    @flow()
+    def flow_move_south(self) -> None:
+        with may_revert():
+            tx = self.maze.moveSouth(*[random_uint64() for _ in range(8)])
+
+    @flow()
+    def flow_move_east(self) -> None:
+        with may_revert():
+            tx = self.maze.moveEast(*[random_uint64() for _ in range(8)])
+
+    @flow()
+    def flow_move_west(self) -> None:
+        with may_revert():
+            tx = self.maze.moveWest(*[random_uint64() for _ in range(8)])
+
+    @invariant(period=100)
+    def invariant_found(self) -> None:
+        discovered.update([i for i in range(1, 49) if self.maze.found(i)])
+        print(discovered)
+        #print("Found ", sum(self.maze.found(i) for i in range(1, 49)))
+
+
+
+@chain.connect()
+def test_maze1():
+    try:
+        test = Maze1FuzzTest().run(100, 1_000_000)
+    finally:
+        print("Discovered ", discovered)
